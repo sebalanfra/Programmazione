3abe21bedcf064930e7c91f156462921c4630220
Seba <sebastiano.lanfranchi@edu.unife.it>
2025-10-14 12:19:50 +0200

    finito :=)

--- programmi_in_c/tipi_var/classMinMaius.c
+++ programmi_in_c/tipi_var/classMinMaius.c
@@ -5,16 +5,16 @@ int main(){
 
     scanf("%c", &a);
 
-    if(a>= 65 && a<= 90){
+    if(a>= 65 && a<= 90)
         printf("Maiuscola\n");
-    }
-    else if(a>=87 && a<=122){
+    
+    else if(a>=97 && a<=122)
         printf("Minuscola\n");
-    }
-    else if(){
+    
+    else if(a>=48 && a<=57)
         printf("Numero\n");
-    }
-    else{
+    
+    else
         printf("Cosa hai scritto bro?\n");
-    }
+    
 }
