33b268f6cebed8a4a51bb80f7cbd59e7dd197487
Seba <sebastiano.lanfranchi@edu.unife.it>
2025-10-21 10:21:00 +0200

    Calcolo della divisione e del resto con utilizzo di Funzioni e Puntatori

---
+++ programmi_in_c/puntatori/divisione.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void divisione(int n1, int n2, int *pq, int *pr){
+
+    *pq = n1/n2; // Quoziente
+
+    *pr = n1%n2; // Resto
+}
+int main(){
+
+    int a = 22, b = 7;
+    int  q , r;
+    divisione(a,b,&q, &r);
+
+    printf("Quoziente : %d\n", q);
+    printf("Resto : %d\n", r);
+
+}
