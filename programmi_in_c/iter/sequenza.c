f24b30227c142a5d189b59f10d6208432a7c73ae
Seba <sebastiano.lanfranchi@edu.unife.it>
2025-10-22 12:22:45 +0200

    six-seven

---
+++ programmi_in_c/iter/sequenza.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    for(int i = 0; i<=67; i++){
+        printf("%d\n", i);
+    }
+
+    printf("Sequenza da 10 a 5\n");
+    
+    for(int i = 10; i>=5; i-- ){
+        printf("%d\n", i);
+    }
+}
