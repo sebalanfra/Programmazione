99c966e252366019bac1a14c85a08ce5c57cba47
Seba <sebastiano.lanfranchi@edu.unife.it>
2025-10-23 15:15:09 +0200

    :)

--- programmi_in_c/iter/divisione.c
+++ programmi_in_c/iter/divisione.c
@@ -7,9 +7,13 @@ int main(){
     scanf("%d%d", &divid, &divis);
 
     m = divid;
-    
+
     while(m>=divis){
         m -= divis;
+        q = q+1;
     }
 
+    printf("Quoziente : %d\n", q);
+    printf("Resto: %d\n", m);
+    
 }
