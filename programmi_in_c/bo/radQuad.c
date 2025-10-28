18f5fff16a0402314589c615d7e8369520a858a2
Seba <sebastiano.lanfranchi@edu.unife.it>
2025-10-28 12:14:14 +0100

    :)

--- programmi_in_c/bo/radQuad.c
+++ programmi_in_c/bo/radQuad.c
@@ -3,14 +3,14 @@ int main(){
 
     float a;
 
-    pintf("Inserisci un numero reale: \n");
+    printf("Inserisci un numero reale: \n");
     scanf("%f", &a);
 
-    int x = 1;
+    float x = 1;
 
     while(x*x!=a){
         x = (x+(a/x))/2;
     }
 
-    printf("%d", x);
+    printf("%f", x);
 }
