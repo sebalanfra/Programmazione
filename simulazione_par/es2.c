9f6e33d6c37230f40e0460acb2c6854945042dc7
Seba <sebastiano.lanfranchi@edu.unife.it>
2025-10-30 09:52:32 +0100

    utilizzo del debugger

--- simulazione_par/es2.c
+++ simulazione_par/es2.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 /*
 Vuole sapere se sappiamo utilizzare il debugger
-vuole che verifichiamo 
+vuole che verifichiamo quale è il primo valore da aggiungere con i = 44122
+RISPOSTA:
+Essendo 44122 non divisibile per 4 aggiunge ad i 3 facendolo diventare 44125
 */
 int main(void){
 
