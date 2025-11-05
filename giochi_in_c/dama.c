f5b52dfa23d89ecc5f6d8375dbfc0ef0845549a3
Seba <sebastiano.lanfranchi@edu.unife.it>
2025-11-05 12:56:00 +0100

    La funzione CREA è stata eliminata e bisognera farne una totalmente nuova

--- giochi_in_c/dama.c
+++ giochi_in_c/dama.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 // Definizione della Grandezza del Campo da giuoco
-#define N 11
-#define M 11
+#define N 24
 
 /* 
     I campi comuni sono da 64 caselle (8x8) quindi se faccio con una matrice :
@@ -10,32 +9,15 @@
    | |
    vuol dire che per ogni i%2 == 0 ci va '-' e per ogni j%2 == 0 ci va '|'
 */
-void crea(char matrice[N][M]){
+void crea(char matrice[N][N]){
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (i == N - 1) {
-                if (j % 2 == 0) {
-                    matrice[i][j] = '|';
-                } else {
-                    matrice[i][j] = ' ';
-                }
-            } else {
-                if (j % 2 == 0) {
-                    matrice[i][j] = '|';
-                } else {
-                    matrice[i][j] = ' ';
-                }
-            }
-        }
-    }
 }
-void stampa(char matrice[N][M]) { 
+void stampa(char matrice[N][N]) { 
 
     system("clear"); 
     printf("         DAMA\n");
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
+        for (int j = 0; j < N; j++) {
             printf("%c", matrice[i][j]);
         }
         printf("\n");
@@ -44,7 +26,7 @@ void stampa(char matrice[N][M]) {
 void gioco();
 int main(){
 
-    char mat[N][M];
+    char mat[N][N];
 
     crea(mat);
     stampa(mat);
