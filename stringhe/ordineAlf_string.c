1d560bd7d401ebe3a9a9068dafffa5456b4e2562
Seba <sebastiano.lanfranchi@edu.unife.it>
2025-11-12 11:41:17 +0100

    nn ho capito cosa fa ??

---
+++ stringhe/ordineAlf_string.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#define d
+
+int compare(char *s1,char *s2){
+
+    return(s1[0]- s2[0]);
+    
+}
+int main(){
+
+    char a[81], b[81];
+
+    printf("Inserisci due parole : \n");
+    scanf("%s%s", a, b);
+
+    int ris = compare(a,b);
+
+    if(ris <0){
+        printf("%s precede %s", a, b);
+    }
+    else if( ris == 0)
+        printf("%s e %s sono identiche", a, b);
+    else   
+        printf("%s precede %s", b, a);
+}
