59b334908193552e4bca15a91ac3d68a61dc9f53
Seba <sebastiano.lanfranchi@edu.unife.it>
2025-11-12 12:21:23 +0100

    Funzione per trasformare una stringa da lettere MAIUSC a minusc

---
+++ stringhe/upperCase.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void toUpperCase(char *s){
+
+    // +- 32 per trasformare tra MAIUSCOLE e/0 minuscole 
+    for(int i = 0;i!='\0'; i++){
+        if(s[i]>= 97 && s[i] <=122){ // oppure s[i] >= 'a' || s[i] <= 'z'
+            s[i]-=32; // oppure s[i] -=('a'-'A');
+        }
+    }
+}
+
+int main(){
+
+    char s[81];
+    scanf("%s", s);
+    toUpperCase(s);
+    printf("%s", s);
+}
